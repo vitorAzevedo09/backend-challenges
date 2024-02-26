ca6a3bb92ea49e7ed7a4712f81abe10fe7dfb56b
Vitor Silva <vitor.azevedo009@gmail.com>
2024-02-26 10:57:08 -0300

    Feat(planet): get One planet by id

--- challenger-amedigital/src/main/java/com/amedigital/challengeramedigital/api/controller/PlanetController.java
+++ challenger-amedigital/src/main/java/com/amedigital/challengeramedigital/api/controller/PlanetController.java
@@ -3,8 +3,12 @@ package com.amedigital.challengeramedigital.api.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.amedigital.challengeramedigital.api.assembler.PlanetAssembler;
 import com.amedigital.challengeramedigital.api.dto.PlanetWithID;
@@ -25,4 +29,12 @@ public class PlanetController {
         .map(planet -> planetAssembler.toDto(planet));
   }
 
+  @GetMapping("/planets/{id}")
+  public ResponseEntity<PlanetWithID> getOne(
+      @PathVariable final Long id) {
+    return planetService.findOne(id)
+        .map(planet -> ResponseEntity.ok(planetAssembler.toDto(planet)))
+        .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND,
+            String.format("Planet with id %d not found in database", id)));
+  }
 }
